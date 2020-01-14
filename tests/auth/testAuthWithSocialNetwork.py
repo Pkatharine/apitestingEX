0437c91f1bb3115e5f83e64c77f1f2b7044988aa
Masha_Pertusiak <57170071+mashapetrusiak@users.noreply.github.com>
2020-01-14 15:26:56 +0200

    add allure

--- tests/auth/testAuthWithSocialNetwork.py
+++ tests/auth/testAuthWithSocialNetwork.py
@@ -11,9 +11,9 @@ class TestAuth(unittest.TestCase):
     def setUpClass(cls):
         pass
 
+    @allure.suite('Tests for loging with social network')
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.link("https://eventsexpress20200103054152.azurewebsites.net/home/events?page=1", name='Click me')
-    @allure.story('Test loging with Facebook as user')
     def test_login_as_user_with_facebook(self):
         response_decoded_json = requests.post(UrlAuth.url_login_fb, data=json.dumps(AuthPayloads.payload_user),
                                               headers=Header.header)
@@ -25,9 +25,9 @@ class TestAuth(unittest.TestCase):
         with allure.step("Verification status code"):
             self.assertEqual(200, response_decoded_json.status_code, "You have BAD REQUEST")
 
+    @allure.suite('Tests for loging with social network')
     @allure.link("https://eventsexpress20200103054152.azurewebsites.net/home/events?page=1", name='Click me')
     @allure.story('Test loging with Facebook as admin')
-    @allure.severity(allure.severity_level.CRITICAL)
     def test_login_as_admin_with_facebook(self):
         response_decoded_json = requests.post(UrlAuth.url_login_fb, data=json.dumps(AuthPayloads.payload_admin),
                                               headers=Header.header)
@@ -39,9 +39,9 @@ class TestAuth(unittest.TestCase):
         with allure.step("Verification status code"):
             self.assertEqual(200, response_decoded_json.status_code, "You have BAD REQUEST")
 
+    @allure.suite('Tests for loging with social network')
     @allure.link("https://eventsexpress20200103054152.azurewebsites.net/home/events?page=1", name='Click me')
     @allure.story('Test loging with Google as admin')
-    @allure.severity(allure.severity_level.CRITICAL)
     def test_login_with_google(self):
         response_decoded_json = requests.post(UrlAuth.url_login_google, data=json.dumps(AuthPayloads.payload_admin),
                                               headers=Header.header)
@@ -53,5 +53,5 @@ class TestAuth(unittest.TestCase):
         pass
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     unittest.main()
